880fd80e0e2fc15d968f4d139e89f8c060a623ba
1x2kb <1x2k@proton.me>
2023-04-05 22:48:20 -0500

    Refactored for clarity

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,10 @@ pub fn conjugate_collection(sequence: &Vec<u8>) -> Vec<u8> {
         vec.push(num);
     }
 
-    vec
+    vec.into_iter().collect()
 }
 
 pub fn get_bean_representation(sequence: &Vec<u8>) -> Vec<String> {
-    println!("Printing bean representation of {:?}", &sequence);
-
     sequence
         .into_iter()
         .map(|row_item| {
@@ -55,6 +53,14 @@ mod get_bean_representation_should {
         let expected = vec!["ooo", "ooo", "o", "o"];
         assert_eq!(result, expected);
     }
+
+    #[test]
+    fn return_correct_orientation_num_gapped_example() {
+        let vec = vec![8, 7, 4, 1];
+        let result = get_bean_representation(&vec);
+        let expected = vec!["oooo", "ooo", "ooo", "ooo", "oo", "oo", "oo", "o"];
+        assert_eq!(result, expected);
+    }
 }
 
 #[cfg(test)]
@@ -73,5 +79,12 @@ mod conjugate_collection_should {
         let vec = vec![4, 2, 2];
         let result = conjugate_collection(&vec);
         assert_eq!(result, vec![3, 3, 1, 1]);
+    }
+
+    #[test]
+    fn conjugate_num_gapped_example() {
+        let vec = vec![8, 7, 4, 1];
+        let result = conjugate_collection(&vec);
+        assert_eq!(result, vec![4, 3, 3, 3, 2, 2, 2, 1]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,41 @@
 use sequence_bean;
 
 fn main() {
-    let vec: Vec<u8> = vec![4, 1, 7, 8];
-    println!("Recieved: {:?}", &vec);
-    let sorted_vec = sort_sequence(vec); // Does not assume sorted form.
-    println!("Sorted to form: {:?}", &sorted_vec);
-    println!("Bean sequence:");
-    let original_bean_sequence = sequence_bean::get_bean_representation(&sorted_vec);
-
-    sequence_bean::print_bean_representation(&original_bean_sequence);
-
-    let conjugate = sequence_bean::conjugate_collection(&sorted_vec);
-    println!("Conjugated form: {:?}", &conjugate);
+    let first_sequence = run_example_1_set();
+    let second_sequence = run_example_2_set();
 }
 
-fn sort_sequence(sequence: Vec<u8>) -> Vec<u8> {
-    let mut copy = sequence.clone();
+fn run_example_1_set() -> Vec<u8> {
+    let sequence = vec![5, 3, 1];
+    run_set_for_sequence(sequence)
+}
+
+fn run_example_2_set() -> Vec<u8> {
+    let sequence = vec![4, 2, 2];
+    run_set_for_sequence(sequence)
+}
+
+fn run_set_for_sequence(sequence: Vec<u8>) -> Vec<u8> {
+    // Rules require a non increasing list. Sorting (dec) prevents such an example from flowing through
+    let sorted_sequence = sort_sequence(&sequence);
+    let sorted_bean_sequence = sequence_bean::get_bean_representation(&sorted_sequence);
+
+    let conjugate = sequence_bean::conjugate_collection(&sorted_sequence);
+    let conjugate_bean = sequence_bean::get_bean_representation(&conjugate);
+
+    println!("(original, sorted, conjugate)");
+    println!(
+        "({:?}, {:?}, {:?})",
+        &sequence, &sorted_sequence, &conjugate
+    );
+    sequence_bean::print_bean_representation(&sorted_bean_sequence);
+    sequence_bean::print_bean_representation(&conjugate_bean);
+
+    conjugate
+}
+
+fn sort_sequence(sequence: &Vec<u8>) -> Vec<u8> {
+    let mut copy = sequence.clone(); // don't mutate the original sequence.
     copy.sort_by(|a, b| b.partial_cmp(a).unwrap());
 
     copy
